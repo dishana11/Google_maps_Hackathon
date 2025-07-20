3f9ee3111001db43288aeb29bf4544805db57a02
Dishana <128571089+dishana11@users.noreply.github.com>
2025-07-20 13:46:48 +0530

    Create codegenNativeCommands.js

---
+++ web-stubs/codegenNativeCommands.js
@@ -0,0 +1,6 @@
+// Web stub for react-native/Libraries/Utilities/codegenNativeCommands
+// This provides a no-op implementation for web builds
+
+module.exports = function codegenNativeCommands() {
+  return {};
+};
